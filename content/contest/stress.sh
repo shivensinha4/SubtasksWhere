88b22d5384c1b57231c2d8214d8f768ffcbb8c9a
Shiven Sinha <shivensinha4@gmail.com>
2024-12-23 01:02:07 +0530

    Updated stress to stop on RTE

--- content/contest/stress.sh
+++ content/contest/stress.sh
@@ -2,6 +2,10 @@ for ((i=1;;i++)); do
   echo $i
   ./random.out
   ./A.out > out1
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+      exit 0
+  fi
   ./B.out > out2
   diff -w out1 out2 || break;
 done
